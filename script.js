6ab175ee0902784975716f3d0c758d84ff080b5a
KelvenB <104724730+KelvenB@users.noreply.github.com>
2023-04-16 13:19:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ moveButton.addEventListener("click", function () {
 });
 
 correctlyButton.addEventListener("click", function() {
-    alert("Chave: bkelven000@gmail.com");
+    alert("Eu pago o lanche e vc o boquet kkk");
 });
 
 
